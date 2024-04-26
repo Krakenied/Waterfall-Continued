0440883cc49faf464512c2f253b022cc276f07ad
Krakenied <Krakenied1@gmail.com>
2024-04-26 18:42:18 +0200

    Fix modules updater

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,14 @@
 
 git submodule update --init && ./scripts/applyPatches.sh || exit 1
 
+if [[ ! -z "${BUILD_NUMBER}"]]; then
+    build_number="${BUILD_NUMBER}"
+else
+    build_number="unknown"
+fi
+
+echo "Running with build number set to $build_number"
+
 if [ "$1" == "--jar" ]; then
-     mvn clean package
+     mvn clean package -Dbuild.number=$build_number
 fi
